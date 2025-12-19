86464f2a4ca626c0a0122e21c8abad590d8c9e2c
kkonzsaki <mekmek23@yahoo.com>
2025-12-19 10:46:20 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://penny-options-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
