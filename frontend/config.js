ab8a534395036ae7ccc65814e688baa1842a7868
kkonzsaki <mekmek23@yahoo.com>
2025-12-26 00:29:03 -0500

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,13 +2,8 @@
    GLOBAL CONFIG
 =========================== */
 
-// CHANGE THIS if backend is remote
-const API_BASE = "http://localhost:5000";
-
-// Scanner settings
+const API_BASE = "http://localhost:5000"; 
 const SCANNER_INTERVAL_MS = 15000;
 
-// Validation
-if (!API_BASE) {
-  console.error("API_BASE is not defined");
-}
+// Debug
+console.log("API_BASE =", API_BASE);
